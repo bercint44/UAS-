f453658f339708fddf42908e10838b71c6929dc3
bercint44 <149394449+bercint44@users.noreply.github.com>
2024-01-09 17:46:53 +0700

    Server.java

--- Server.java
+++ Server.java
@@ -1,17 +1,17 @@
 import java.io.*;
 import java.net.*;
 
-public class Client {
+public class Server {
     public static void main(String[] args) {
         try {
             // Buat BufferedReader untuk membaca input dari pengguna
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
-            // Meminta pengguna untuk memasukkan alamat IP dan port
-            System.out.print("Masukkan alamat IP server: ");
-            String serverIP = userInput.readLine();
-            System.out.print("Masukkan port server: ");
-            int serverPort = Integer.parseInt(userInput.readLine());
+            // Meminta pengguna untuk memasukkan port
+            System.out.print("Masukkan port untuk server: ");
+            int port = Integer.parseInt(userInput.readLine());
 
-            // Buat socket untuk terhubung ke server dengan alamat IP dan port yang ditentukan pengguna
-            Socket socket = new Socket(serverIP, serverPort);
+            // Buat server socket dengan port yang ditentukan pengguna
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            System.out.println("Server berjalan, menunggu koneksi...");
